45ffcc84952b58b65cc2f52acf402c58e43977cd
Qin Guan <qinguan20040914@gmail.com>
2025-07-28 22:17:17 +0800

    Update

--- GroundZero.Web/Entities/Hackathon.cs
+++ GroundZero.Web/Entities/Hackathon.cs
@@ -2,6 +2,7 @@ using SqlSugar;
 
 namespace GroundZero.Web.Entities;
 
+[SugarIndex("IX_Hackathon_ShortCode", nameof(ShortCode), OrderByType.Asc)]
 public class Hackathon
 {
     [SugarColumn(IsPrimaryKey = true)]
@@ -14,6 +15,22 @@ public class Hackathon
     public string Venue { get; set; } = null!;
 
     public string HomepageUri { get; set; } = null!;
+
+    public string ShortCode { get; set; } = null!;
+
+    /// <summary>
+    /// When enabled, participants will be able to see and join the hackathon using the short code
+    /// </summary>
+    [SugarColumn(OldColumnName = "IsAcceptingParticipants")]
+    public bool IsPublished { get; set; }
+
+    public DateTimeOffset EventStartDate { get; set; }
+
+    public DateTimeOffset EventEndDate { get; set; }
+
+    public DateTimeOffset SubmissionsStartDate { get; set; }
+
+    public DateTimeOffset SubmissionsEndDate { get; set; }
 
     [Navigate(NavigateType.OneToMany, nameof(Participant.HackathonId))]
     public List<Participant> Participants { get; set; }
--- GroundZero.Web/Program.cs
+++ GroundZero.Web/Program.cs
@@ -1,9 +1,12 @@
+using System.Security.Claims;
 using AspNet.Security.OAuth.GitHub;
 using FastEndpoints;
 using FastEndpoints.Swagger;
-using GroundZero.Web.Authentication;
 using GroundZero.Web.Components;
 using GroundZero.Web.Context;
+using GroundZero.Web.Entities;
+using Havit.Blazor.Components.Web;
+using Havit.Blazor.Components.Web.Bootstrap;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Scalar.AspNetCore;
@@ -12,6 +15,10 @@ using SqlSugar;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddHxServices();
+builder.Services.AddHxMessenger();
+builder.Services.AddHxMessageBoxHost();
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
@@ -44,8 +51,6 @@ builder.Services.AddSingleton<ISqlSugarClient>(sp =>
     return sqlSugar;
 });
 
-builder.Services.AddTransient<IClaimsTransformation, GitHubClaimsTransformer>();
-
 builder.Services
     .AddAuthentication(options =>
     {
@@ -58,6 +63,31 @@ builder.Services
         options.ClientId = "Iv23lieaI0YRJGwxCDUb";
         options.ClientSecret = "";
         options.CallbackPath = "/Auth/GitHub/Callback";
+
+        options.Events.OnCreatingTicket = async context =>
+        {
+            var userName = context.Principal?.Claims.First(c => c.Type == ClaimTypes.Name).Value;
+            ArgumentNullException.ThrowIfNull(userName);
+
+            var db = context.HttpContext.RequestServices.GetRequiredService<ISqlSugarClient>();
+
+            var account = await db.Queryable<GitHubOnlineAccount>()
+                .Includes(a => a.User)
+                .SingleAsync(a => a.UserName == userName);
+
+            if (account is null)
+            {
+                account = await db.InsertNav(new GitHubOnlineAccount
+                    {
+                        UserName = userName,
+                        User = new User { Name = userName }
+                    })
+                    .Include(g => g.User)
+                    .ExecuteReturnEntityAsync();
+            }
+
+            context.Identity?.AddClaim(new Claim(ClaimTypes.NameIdentifier, account.UserId.ToString()));
+        };
     });
 
 builder.Services
@@ -88,7 +118,6 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.UseHttpsRedirection();
-app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
@@ -100,9 +129,26 @@ app.UseAuthentication()
     .UseAuthorization()
     .UseFastEndpoints();
 
+app.UseAntiforgery();
+
 app.UseOpenApi(c => c.Path = "/openapi/{documentName}.json");
 
 app.MapScalarApiReference();
-// app.MapIdentityApi<AppUser>();
+app.MapGet("/Auth/GitHub/Login",
+    async (context) =>
+    {
+        await context.ChallengeAsync(new AuthenticationProperties
+        {
+            RedirectUri = "/"
+        });
+    });
+app.MapGet("/Auth/Logout",
+    async (context) =>
+    {
+        await context.SignOutAsync(new AuthenticationProperties
+        {
+            RedirectUri = "/"
+        });
+    });
 
 app.Run();
