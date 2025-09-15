8d5c1db26c5bf1a1e4d1fe73ae6e5158355a3c09
Qin Guan <qinguan20040914@gmail.com>
2025-09-15 13:43:21 +0800

    AAAAAAAAAAAAAAAAAAAAAAAAAAA

--- GroundZero.Web/Authentication/AuthenticationExtensions.cs
+++ GroundZero.Web/Authentication/AuthenticationExtensions.cs
@@ -7,7 +7,7 @@ public static class AuthenticationExtensions
     public static Guid GetUserId(this ClaimsPrincipal principal)
     {
         var raw = principal.Claims
-            .Single(c => c.Type == ClaimTypes.NameIdentifier && c.Issuer == ClaimsIdentity.DefaultIssuer);
+            .Single(c => c is { Type: ClaimTypes.NameIdentifier, Issuer: ClaimsIdentity.DefaultIssuer });
 
         return Guid.Parse(raw.Value);
     }
